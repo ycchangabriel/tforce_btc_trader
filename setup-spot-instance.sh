03b62b40aecc62f9c865f43c45e93a596cf1c76e
Gabriel Chan <ycchangabriel@gmail.com>
2018-12-30 18:51:56 +0000

    wip

--- setup-spot-instance.sh
+++ setup-spot-instance.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-# this script assumes using aws ubuntu deep learning image
+# this script assumes using aws ubuntu 18.04
+sudo apt update
 
-# start virtual environment before running this script!
+# install python 3.6
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt update
+sudo apt install -y python3.6
+
+# install virtualenv
+sudo apt install -y virtualenv
+virtualenv -p python3 venv
+source ./venv/bin/activate
 
 # setup config.json
 cp config.example.json config.json
 # input the remote database!
 
-# first things first, update
-sudo apt update
-
 # check and install python 3.6
 # TODO
 
-# install dependencies
-pip install -r requirements-freeze.txt
+# install program dependencies
+cd /usr/local/src/ && \
+    sudo wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz && \
+    sudo tar -xvzf ta-lib-0.4.0-src.tar.gz && \
+    cd ta-lib && \
+    sudo ./configure --prefix=/usr && \
+    sudo make install
+sudo apt install -y build-essential
+sudo apt install -y python3.6-dev
+pip install -r requirements-prerequisite.txt
+pip install -r requirements-gpu.txt
 
 # clone the latest tensorforce (as this script was created, the commit was 9aed7684173848b8146a7b9c73a612e3f6dca2f5)
 git clone https://github.com/lefnire/tensorforce.git
